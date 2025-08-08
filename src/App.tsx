b8d2947f26a0cec6d964cc869a24dcd1d2d6dcbc
Anderson Sinaluisa <anderson.sinaluisa@siigo.com>
2025-08-07 23:56:57 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
 import InversifyProvider from './components/InversifyProvider'
-
+import { Toaster } from './components/ui/toaster'
 function App() {
 
   return (
     <InversifyProvider>
       <RouterProvider router={router} />
+      <Toaster />
 
     </InversifyProvider>
 
